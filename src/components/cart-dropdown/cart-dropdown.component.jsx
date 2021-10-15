e767ac21b473d256550c17054e1da61d1cabf70f
Nathan Azevedo <nathan@Nathans-MacBook-Pro.local>
2021-10-15 15:53:40 -0700

    updating with reselect, replaced cart selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import CustomButton from '../custom-button/custom-button.component'
+import CartItem from '../cart-item/cart-item.component'
+import {connect} from 'react-redux'
+import {selectCartItems} from '../../redux/cart/cart.selectors'
 
 import './cart-dropdown.styles.scss'
 
-const CartDropDown = () => (
+const CartDropDown = ({cartItems}) => (
     <div className='cart-dropdown'>
-        <div className="cart-items" />
+        <div className="cart-items" >
+            {
+                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+            }
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
-export default CartDropDown
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
+})
+
+
+
+
+export default connect(mapStateToProps)(CartDropDown)
